0ed923c822930665eb15a1370c465a6628698667
leonknight99 <leonknight99@outlook.com>
2021-04-09 15:46:28 +0100

    Added more graph outputs

--- GraphNN/GraphTools.py
+++ GraphNN/GraphTools.py
@@ -4,13 +4,16 @@ import numpy as np
 import matplotlib.pyplot as plt
 import scikitplot
 import sklearn
+from sklearn.metrics import roc_auc_score, roc_curve
 
 np.set_printoptions(linewidth=200)
-
 directory = os.path.join(os.getcwd(), 'GNNout')
 
 
 def file_list():
+
+    # List of input GNN attempts, removing unwanted files from the list such as: previous image outputs and MacOS file
+
     input_files = sorted(os.listdir(directory))
     input_files = [x for x in input_files if '.png' not in x]
     try:
@@ -44,6 +47,7 @@ def plot_history(loss_values, val_loss_values, accuracy_values, val_accuracy_val
     axs[1].legend()
 
     for ax in axs.flat:
+        ax.minorticks_on()
         ax.set(xlabel='Epochs', ylabel='Loss')
 
     plt.savefig(f'{directory}/{data_number}training_history.png', dpi=600)
@@ -81,13 +85,13 @@ def plot_sig_back_cumulative(predictions, targets, data_number):
     signal_cumulative = np.cumsum(signal_predictions)
     background_cumulative = np.cumsum(background_predictions)
 
-    fig, ax = plt.subplots(figsize=(8, 4))
+    fig, ax = plt.subplots(figsize=(5, 5))
 
     n, bins, patches = ax.hist(signal_predictions, bins=100, density=True, histtype='step', cumulative=True, label='Signal')
     patches[0].set_xy(patches[0].get_xy()[:-1])
     n, bins, patches = ax.hist(background_predictions, bins=bins, density=True, histtype='step', cumulative=-1, label='Background' )
     patches[0].set_xy(patches[0].get_xy()[1:])
-
+    ax.minorticks_on()
     ax.legend()
     ax.set_xlabel('Probability events are classed as signal')
     ax.set_ylabel('Cumulative sum of events')
@@ -100,32 +104,79 @@ def plot_sig_sqrt_back(predictions, targets, data_number):
 
     # Plot of how the s/sqrt(b) changes when the prediction cut changes
 
-    cuts = np.linspace(0, 1, 1001)
+    thresholds = np.linspace(0, 1, 1001)
     sb_list = []
 
-    for cut in cuts:
+    for threshold in thresholds:
         combined = np.vstack((predictions, targets)).T
-        combined = combined[combined[:, 0] > cut]
+        combined = combined[combined[:, 0] > threshold]
         n_signal = np.count_nonzero(combined[:, 1] == 1)
         n_background = np.count_nonzero(combined[:, 1] == 0)
         sig = n_signal / np.sqrt(n_background)
         sb_list.append(sig)
-    plt.plot(cuts, sb_list)
+
+    fig, ax = plt.subplots(figsize=(5, 5))
+    ax.plot(thresholds, sb_list)
+    ax.minorticks_on()
+    ax.set_xlabel('Threshold')
+    ax.set_ylabel(r'$s/\sqrt{b}$')
 
     plt.savefig(f'{directory}/{data_number}sig_sqrt_back.png', dpi=600)
     plt.clf()
 
 
-def plot_roc_curve(predictions, targets, data_number):
-    print(type(np.asarray(predictions)))
-    print(type(np.asarray(targets)))
-    fpr, tpr, thresholds = sklearn.metrics.roc_curve(targets, predictions)
-    print(fpr)
-    print(tpr)
-    print(thresholds)
-    scikitplot.metrics.plot_roc(targets, predictions)
-    plt.savefig(f'{directory}/{data_number}roc_curve.png', dpi=600)
+def plot_roc_curve_plt(predictions, targets, data_number):
+
+    # Plotting a Receiver Operating Characteristic (ROC) curve and working out the Area Underneath the Curve (AUC)
+
+    thresholds = np.linspace(0, 1, 1001)
+
+    tpr_list, fpr_list = [], []
+
+    for threshold in thresholds:
+        combined = np.vstack((predictions, targets)).T
+        tttt = combined[combined[:, 0] > threshold]
+        tt = combined[combined[:, 0] < threshold]
+        tp = np.count_nonzero(tttt[:, 1] == 1)  # True positives
+        fp = np.count_nonzero(tttt[:, 1] == 0)  # False positives
+        tn = np.count_nonzero(tt[:, 1] == 0)  # True negative
+        fn = np.count_nonzero(tt[:, 1] == 1)  # False negative
+
+        tpr = tp / (tp + fn)
+        fpr = fp / (fp + tn)
+
+        tpr_list.append(tpr)
+        fpr_list.append(fpr)
+
+    plt.plot(fpr_list, tpr_list)
+    plt.plot(fpr_list, fpr_list, linestyle='--')
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.savefig(f'{directory}/{data_number}roc_curve_plt.png', dpi=600)
     plt.clf()
+
+
+def plot_roc_curve_skl(predictions, targets, data_number):
+
+    # Plotting a ROC curve using the scikit learn Python library
+
+    true_values = targets[0].astype(int)
+    probabilities = predictions[0].astype(float)
+
+    fpr, tpr, thresholds = roc_curve(true_values, probabilities)
+    auc = roc_auc_score(true_values, probabilities)
+
+    fig, ax = plt.subplots(figsize=(5, 5))
+    ax.set_aspect('equal')
+    ax.plot(fpr, tpr, label='DNN AUC = %0.4f' % auc)
+    ax.plot(fpr, fpr, linestyle='--', label='Random AUC = 0.5')
+    ax.minorticks_on()
+    ax.set_xlabel('False Positive Rate')
+    ax.set_ylabel('True Positive Rate')
+    plt.legend()
+    plt.savefig(f'{directory}/{data_number}roc_curve_skl.png', dpi=600)
+    plt.clf()
+
 
 files = file_list()
 for j in range(files.shape[0]):
@@ -145,5 +196,6 @@ for j in range(files.shape[0]):
                  int(data_info['Best Value Epoch']), data_info['t0'])
     plot_sig_back_hist(predictions, targets, data_info['t0'])
     plot_sig_back_cumulative(predictions, targets, data_info['t0'])
-    #plot_roc_curve(predictions, targets, data_info['t0'])
+    plot_roc_curve_plt(predictions, targets, data_info['t0'])
     plot_sig_sqrt_back(predictions, targets, data_info['t0'])
+    plot_roc_curve_skl(predictions, targets, data_info['t0'])
